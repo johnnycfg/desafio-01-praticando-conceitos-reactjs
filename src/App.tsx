45d9fe57a837621144cf093e246ca8e31458df63
Johnny Ferraz <johnny.chr@hotmail.com>
2022-11-15 15:20:42 -0300

    add, complete and delete actions created

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,55 @@ import './styles/global.css';
 
 import ClipboardImg from './assets/Clipboard.svg';
 import { TaskListItem } from './components/TaskListItem/TaskListItem';
+import { ChangeEvent, useState } from 'react';
 
+
+interface ITask {
+  title: string;
+  completed: boolean;
+}
 
 function App() {
-  const taskListEmpty = false;
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [newTaskValue, setNewTaskValue] = useState('');
+
+  const isTaskListEmpty = tasks?.length > 0;
+  const tasksAmount = tasks.length;
+  const completedTasksAmount = tasks.filter(task => task.completed).length;
+
+  function handleNewTaskValueChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskValue(event.target.value);
+  }
+
+  function handleAddNewTask() {
+    setTasks([...tasks, {title: newTaskValue, completed: false}]);
+    setNewTaskValue('')
+  }
+
+  function handleChangeTaskStatus(taskTitle: string) {
+    const newTasksArray = tasks.map(task => {
+      if (taskTitle === task.title) {
+        return { title: task.title, completed: !task.completed }
+      }
+
+      return task;
+    })
+
+    setTasks(newTasksArray)
+  }
+
+  function handleDeleteTask(taskTitle: string) {
+    const newTasksArray = tasks.filter(task => {
+      if (taskTitle !== task.title) {
+        return true
+      }
+
+      return false;
+    })
+
+    setTasks(newTasksArray)
+  }
+  
 
   return (
    <div>
@@ -16,8 +61,13 @@ function App() {
 
       <div className={styles.wrapper}>
         <div className={styles.addTask}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>
+          <input 
+            type="text" 
+            placeholder="Adicione uma nova tarefa"  
+            value={newTaskValue}
+            onChange={handleNewTaskValueChange}
+          />
+          <button onClick={handleAddNewTask}>
             Criar
             <PlusCircle size={20} />
           </button>
@@ -27,16 +77,16 @@ function App() {
           <div className={styles.taskListHeader}>
             <div className={styles.taskListHeaderCreated}>
               <p>Tarefas criadas</p>
-              <span>0</span>
+              <span>{tasksAmount}</span>
             </div>
             <div className={styles.taskListHeaderCompleted}>
               <p>Tarefas concluídas</p>
-              <span>0</span>
+              <span>{`${completedTasksAmount} de ${tasksAmount}`}</span>
             </div>
           </div>
 
           <div className={styles.taskListItems}>
-            {taskListEmpty ? (
+            {!isTaskListEmpty ? (
               <>
                 <img src={ClipboardImg} />
                 <p>Você ainda não tem tarefas cadastradas</p>
@@ -44,7 +94,17 @@ function App() {
               </>
             ) : (
               <>
-                <TaskListItem />
+                {tasks && tasks.map((task, index) => {
+                  return (
+                    <TaskListItem 
+                      key={index} 
+                      title={task.title} 
+                      completed={task.completed} 
+                      handleChangeTaskStatus={handleChangeTaskStatus}
+                      handleDeleteTask={handleDeleteTask}
+                    />
+                  )
+                })}
               </>
             )}
           </div>
--- src/components/TaskListItem/TaskListItem.tsx
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -4,12 +4,23 @@ import { Trash } from "phosphor-react";
 
 import styles from "./TaskListItem.module.css";
 
-export function TaskListItem() {
+interface ITask {
+  title: string;
+  completed: boolean;
+  handleChangeTaskStatus: (title: string) => void;
+  handleDeleteTask: (title: string) => void;
+}
+
+export function TaskListItem({ title, completed, handleChangeTaskStatus, handleDeleteTask }: ITask) {
   return (
     <div className={styles.wrapper}>
-      <input type="checkbox" />
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-      <button>
+      <input 
+        type="checkbox" 
+        checked={completed} 
+        onChange={() => handleChangeTaskStatus(title)} 
+      />
+      <p>{title}</p>
+      <button onClick={() => handleDeleteTask(title)}>
         <Trash size="24" />
       </button>
     </div>
